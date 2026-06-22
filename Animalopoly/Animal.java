18a6cc644e0263867362a6eaa65ae146e120af91
BHASVIC-SebTaylor25 <seb.taylor25@students.bhasvic.ac.uk>
2026-06-22 15:33:30 +0100

    added animal code

--- Animalopoly/Animal.java
+++ Animalopoly/Animal.java
@@ -8,12 +8,50 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Animal extends Actor
 {
+    String name;
+    int cost;
+    int level;
+    int visit;
+    boolean free;
     /**
      * Act - do whatever the Animal wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
+    public Animal(String animalName, int animalCost, GreenfootImage icon){
+        setImage(icon);
+        GreenfootImage myImage = getImage();
+        getImage().scale(myImage.getHeight()*1, myImage.getWidth()*1); //can be changed if image is too big or small
+        //1 can be changed with a variable and passed into constructor so size can be controlled when adding animals
+        this.name = animalName;
+        this.cost = animalCost;
+        this.level = 1;
+        this.visit = (int)this.cost/4;
+        this.free = false;
+        //when animal is added, it is added to a specific square using coords and given these characteristics through parameters
+    }
+    
+    public int getCost(){
+        return this.cost;
+    }
+    public int getVisit(){
+        return this.visit;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public boolean getFree(){
+        return this.free;
+    }
+    
+    public void upgrade()
     {
-        // Add your action code here.
+        level++;
+        visit+= (int)this.cost/4;
+        //increases level of animal and increases visit by inital visit, cost is constant
+        //this doesnt involve taking money from the players
+    }
+    public void isBought(){
+        this.free = true;
+        //if bought, call this so the animal is no longer free to buy
     }
 }
